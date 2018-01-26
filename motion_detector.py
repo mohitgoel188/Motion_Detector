6d4c81144ed5be228e64476769b549ef4b1acb0d
mohitgoel188 <35655238+mohitgoel188@users.noreply.github.com>
2018-01-26 18:34:52 +0530

    Main Commit
    Main File

---
+++ motion_detector.py
@@ -0,0 +1,53 @@
+import cv2,pandas
+from datetime import datetime
+
+video=cv2.VideoCapture(0)
+first_frame=None
+status_list=[None,None]
+times=[]
+df=pandas.DataFrame(columns=['Start','End'])
+while True:
+    check,frame=video.read()
+    status=0
+
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray=cv2.GaussianBlur(gray,(21,21),0)
+
+    if first_frame is None:
+        first_frame=gray
+        continue
+    delta_frame=cv2.absdiff(first_frame,gray)
+    thresh_frame=cv2.threshold(delta_frame,30,255,cv2.THRESH_BINARY)[1]
+    thresh_frame=cv2.dilate(thresh_frame,None,2)
+
+    (_,cnts,_)=cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    for contour in cnts:
+        if cv2.contourArea(contour)<10000:
+            continue
+        status=1
+        (x,y,w,h)=cv2.boundingRect(contour)
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(200,50,50),3)
+    
+    status_list.append(status)
+    if status_list[-2]==0 and status_list[-1]==1:
+        times.append(datetime.now())
+    if status_list[-2]==1 and status_list[-1]==0:
+        times.append(datetime.now())
+        
+    cv2.imshow('Real Image',frame)
+    cv2.imshow('Threshold Image',thresh_frame)
+    cv2.imshow('GrayScale Image',gray)
+    cv2.imshow('Delta Image',delta_frame)
+    key=cv2.waitKey(1)
+    if key==ord('q'):
+        if status==1:
+            times.append(datetime.now())
+        break
+
+video.release()
+for time in times:
+    print(times)
+for i in range(0,len(times),2):
+    df=df.append({'Start':times[i],'End':times[i+1]},ignore_index=True)
+df.to_csv('Times.csv')
+cv2.destroyAllWindows
